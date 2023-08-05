2bea0e152a76d51d73448b2e4f074eeb525936c9
PaulLuqueCcosi <pluquec@unsa.edu.pe>
2023-08-05 14:29:57 -0500

    entidades completadas

--- bank_a/src/main/java/sd/grupo1/server/entities/Account.java
+++ bank_a/src/main/java/sd/grupo1/server/entities/Account.java
@@ -1,18 +1,61 @@
 package sd.grupo1.server.entities;
 
+import java.io.Serializable;
+
 /**
  * Debe de representar una cuenta
  * Entro los atributos se tiene
- *  private int acc_num;
+ * private int acc_num;
+ * private int acc_pin;
+ * private int acc_bal;
+ * private int acc_status;
+ * 
+ * 
+ */
+
+public class Account implements Serializable {
+
+    private int acc_num;
     private int acc_pin;
-    private int acc_bal;
+    private double acc_bal;
     private int acc_status;
- 
-    
-    */
 
+    public Account() {
+        acc_num = 0;
+        acc_pin = 0;
+        acc_bal = 0;
+        acc_status = 0;
+    }
 
-public class Account {
-    
-    // TODO: revisar el codigo 1 de la miss, preaticamente seria lo mismo.
+    public int getAcc_num() {
+        return acc_num;
+    }
+
+    public void setAcc_num(int acc_num) {
+        this.acc_num = acc_num;
+    }
+
+    public int getAcc_pin() {
+        return acc_pin;
+    }
+
+    public void setAcc_pin(int acc_pin) {
+        this.acc_pin = acc_pin;
+    }
+
+    public double getAcc_bal() {
+        return acc_bal;
+    }
+
+    public void setAcc_bal(double acc_bal) {
+        this.acc_bal = acc_bal;
+    }
+
+    public int getAcc_status() {
+        return acc_status;
+    }
+
+    public void setAcc_status(int acc_status) {
+        this.acc_status = acc_status;
+    }
 }
--- bank_a/src/main/java/sd/grupo1/server/entities/User.java
+++ bank_a/src/main/java/sd/grupo1/server/entities/User.java
@@ -1,16 +1,66 @@
 package sd.grupo1.server.entities;
 
+import java.util.List;
+
 public class User {
 
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private List<Account> accounts;
 
-    // hibernatem acerigaur al respeto
-    
-    // TODO es un user
-    // debe de tener DNI, nombre, apellido, 
-    // debe de tener una lista de cuentas account
+    public User() {
+        // Constructor vacío requerido por Hibernate
+    }
 
-    // TODO:
-    // deberia de tener User y pass, esto sirve para iniciar sesion en algun banco.
-    
-    
+    public User(int id, String nombre, String apellido, String dni, List<Account> accounts) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.accounts = accounts;
+    }
+
+    // Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
 }
